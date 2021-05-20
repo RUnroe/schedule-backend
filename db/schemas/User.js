d6c0a7bfd2a8933966d55591132cc05694ca8c02
Joe Reed <josephreed2600@gmail.com>
2021-05-19 18:52:33 -0600

    implement DAL schemas, missing some validators prolly

---
+++ db/schemas/User.js
@@ -0,0 +1,20 @@
+const Schema = require('./Schema');
+
+module.exports = new Schema(
+	'Users'
+	, ['id', 'first_name', 'last_name', 'password', 'email'] // keys
+	, ['id'] // requireds
+	, ['first_name', 'last_name'] // nullables
+	, ['id', 'email'] // immutables
+	, ['id'] // automatics
+	, ['id'] // update keys
+	, { // type samples
+		'id': new Long()
+		, 'first_name': ''
+		, 'last_name': ''
+		, 'password': ''
+		, 'email': ''
+	}
+	, [] // validators
+	, true // permit nulls
+);
